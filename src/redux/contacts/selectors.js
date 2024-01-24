e694bc5e8a79c041b71ac68961bceddce1786614
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-01-24 14:53:30 +0200

    fixed selectFilteredContacts

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,17 +1,30 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = store => store.phonebook.contacts.items;
 export const selectFilter = store => store.phonebook.filter;
 export const selectisLoading = store => store.phonebook.contacts.isLoading;
 export const selectError = store => store.phonebook.contacts.error;
 
-export const selectFilteredContacts = store => {
-  const contacts = selectContacts(store);
-  const filter = selectFilter(store);
+// export const selectFilteredContacts = store => {
+//   const contacts = selectContacts(store);
+//   const filter = selectFilter(store);
 
-  console.log('Hi!');
+//   console.log('Hi!');
 
-  return contacts.filter(
-    contact =>
-      contact.name.toLowerCase().includes(filter.trim().toLowerCase()) ||
-      contact.phone.includes(filter)
-  );
-};
+//   return contacts.filter(
+//     contact =>
+//       contact.name.toLowerCase().includes(filter.trim().toLowerCase()) ||
+//       contact.phone.includes(filter)
+//   );
+// };
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    console.log('Hi!');
+    return contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(filter.trim().toLowerCase()) ||
+        contact.phone.includes(filter)
+    );
+  }
+);
